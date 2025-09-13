8c8c90e24becb3e57939fc74b343a60fe5bf5981
Kunal Singh Dadhwal <kunalsinghdadhwal@gmail.com>
2025-09-13 11:50:10 +0530

    thoda auth
    Signed-off-by: Kunal Singh Dadhwal <kunalsinghdadhwal@gmail.com>

--- internal/handlers/auth_handler.go
+++ internal/handlers/auth_handler.go
@@ -1,1 +1,12 @@
 package handlers
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+
